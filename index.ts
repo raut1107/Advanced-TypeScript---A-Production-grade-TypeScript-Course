c3a9346cf9edd8a3514e36ec3812526da1520bfc
sanchita <learningsanchitamean@gmail.com>
2025-10-08 15:55:34 +0530

    first commit for outline of project

---
+++ index.ts
@@ -0,0 +1,6 @@
+type Compute<A extends any> = A extends Function ? A:{[K in keyof A]};
+export type Expect<T extends true> = T;
+export type Equal<X,Y> = (<T>() => T extends Compute<X> ? 1:2) extends <T>
+() => T extends Compute<Y> ? 1:2
+? true
+:false;
